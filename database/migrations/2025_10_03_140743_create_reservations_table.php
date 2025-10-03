94cdf27afb98bc10fa89e266a35cae77e963574a
Samuel <rene28609@gmail.com>
2025-10-03 09:11:23 -0500

    first commit

---
+++ database/migrations/2025_10_03_140743_create_reservations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('court_id')->constrained('courts');
+            $table->date('fecha');
+            $table->time('hora_inicio');
+            $table->integer('duracion_horas');
+            $table->enum('estado', ['pendiente', 'confirmada', 'cancelada'])->default('pendiente');
+            $table->decimal('total_estimado', 10, 2)->nullable();
+            $table->boolean('pagado_bool')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
